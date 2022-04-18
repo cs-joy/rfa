755c2b333f17859babc4ee3ae83fb4d2171c680d
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-19 00:24:23 +0600

    sign up function add with firebase function

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,18 @@
 import { React, useRef } from 'react'
 import { Form, Button, Card} from 'react-bootstrap'
+import { useAuth } from './contexts/AuthContext'
 
 export default function Signup() {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
+  const { signup } = useAuth()
+
+  function handleSubmit(e) {
+      e.preventDefault()
+
+      signup(emailRef.current.value, passwordRef.current.value)
+  }
 
   return (
     <>
