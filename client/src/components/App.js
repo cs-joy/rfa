a6a9fa9fa4877ee82c00ec3be79977b164cc94af
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-19 00:03:29 +0600

    style add

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,19 @@
+import Signup from './Signup'
+import { Container } from 'react-bootstrap'
 
 function App() {
-  return "Hello React World!"
+  return (
+    <>
+      <Container 
+        className='d-flex align-items-center justify-content-center'
+        style={{ minHeight: "100vh" }}
+      >
+        <div className='w-100' style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </>
+  )
 
 }
 
