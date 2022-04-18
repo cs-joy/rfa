346b7191c4715acf56a01468481876ea84b921cc
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-19 00:44:56 +0600

    plAC update

--- client/src/components/contexts/AuthContext.js
+++ client/src/components/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { auth } from '../firebase'
+import { auth } from '../../firebase'
 
 const AuthContext = React.create.AuthContext()
 
